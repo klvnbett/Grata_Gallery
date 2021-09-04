a06ab515e9b62cdfc1ae9f43ec2b32b21e5db46e
klvnbett <kelvin.bett@student.moringaschool.com>
2021-09-04 21:02:54 +0300

    added the search method

--- bgallery/views.py
+++ bgallery/views.py
@@ -11,3 +11,22 @@ def images(request):
     date=dt.date.today()
     users=User.objects.all()
     return render(request,'major/images.html',{'images':images,'date':date})
+
+def search(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        print(search_term)
+        searched_image = Photo.search(search_term)
+        print(searched_image)
+        message = f'{search_term}'
+        params = {
+            'searched_image': searched_image,
+            'message': message,
+        }
+
+        return render(request, 'major/search.html', params)
+
+    else:
+        message = 'Ooppss, You did not search for anything.'
+        return render(request, 'major/search.html', locals())
+
