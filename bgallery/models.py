010e50c247feaa85820fde7b3a77c62914049842
klvnbett <kelvin.bett@student.moringaschool.com>
2021-09-04 14:36:26 +0300

    added static files

--- bgallery/models.py
+++ bgallery/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Photos(models.Model):
+    image=models.ImageField(upload_to='photos/')
+    image_name=models.CharField(max_length=40)
+    image_description=models.TextField(blank=False)
--- gallery/settings.py
+++ gallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER':'kelvin',
+        'PASSWORD':'read432q',
     }
 }
 
@@ -121,8 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
